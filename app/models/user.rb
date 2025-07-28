23604a907b2f45341d4e99bfa66b2cb8dcc8d0bf
ricardo-benicio <rikrdoofelipe@outlook.com>
2025-07-28 09:56:53 -0300

    feat: Setup authentication: JWT service, User/Sessions controllers, API routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    devise :database_authenticatable, :registerable
+    
     has_many :conversations, dependent: :destroy
     has_many :appointments, through: :conversations
 end
